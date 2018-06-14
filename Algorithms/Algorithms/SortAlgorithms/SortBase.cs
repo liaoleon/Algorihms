12f2320237dd3996584df0431d1736a5407364cb
leon <leon@1510009NB02>
2018-06-14 10:57:44 +0800

    更改swap 存取範圍為protected

--- Algorithms/Algorithms/SortAlgorithms/SortBase.cs
+++ Algorithms/Algorithms/SortAlgorithms/SortBase.cs
@@ -4,7 +4,7 @@ namespace Algorithms.SortAlgorithms
 {
     public abstract class SortBase
     {
-        public void Swap<T>(ref T a, ref T b)
+        protected void Swap<T>(ref T a, ref T b)
         {
             T temp = a;
             a = b;
